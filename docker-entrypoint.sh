2d7e325cd59eb996f3e1f64c5572c03e123bf4c6
Cheewai Lai <clai@csir.co.za>
2016-05-17 12:04:56 +0200

    Removed set -e from docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 RUNUSER_UID="${RUNUSER_UID:-1000}"
 RUNUSER_HOME="${RUNUSER_HOME:-/data}"
-set -eux
+set -ux
 adduser -s /bin/false -D -h $RUNUSER_HOME -H -u $RUNUSER_UID runuser
 cd $RUNUSER_HOME
 exec gosu runuser "$@"
